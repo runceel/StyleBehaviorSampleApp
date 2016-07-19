ef2e1a5f0f18ead6bd9f5794237ab76ca390a672
Kazuki Ohta <k_ota28@hotmail.com>
2016-07-19 19:25:11 +0900

    プロジェクト ファイルを追加します。

---
+++ StyleBehaviorSampleApp/AlertBehavior.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace StyleBehaviorSampleApp
+{
+    public class AlertBehavior : Behavior<Button>
+    {
+        public static readonly DependencyProperty MessageProperty =
+            DependencyProperty.Register("Message", typeof(string), typeof(AlertBehavior), new PropertyMetadata(null));
+
+        public string Message
+        {
+            get { return (string)GetValue(MessageProperty); }
+            set { SetValue(MessageProperty, value); }
+        }
+
+        protected override void OnAttached()
+        {
+            this.AssociatedObject.Click += this.Button_Click;
+        }
+
+        protected override void OnDetaching()
+        {
+            this.AssociatedObject.Click -= this.Button_Click;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(this.Message ?? "");
+        }
+
+    }
+}
---
+++ StyleBehaviorSampleApp/StyleBehaviorCollection.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interactivity;
+
+namespace StyleBehaviorSampleApp
+{
+    public class StyleBehaviorCollection : FreezableCollection<Behavior>
+    {
+
+        public static readonly DependencyProperty StyleBehaviorsProperty =
+            DependencyProperty.RegisterAttached(
+                "StyleBehaviors", 
+                typeof(StyleBehaviorCollection), 
+                typeof(StyleBehaviorCollection), 
+                new PropertyMetadata((sender, e) =>
+                {
+                    if (e.OldValue == e.NewValue) { return; }
+
+                    var value = e.NewValue as StyleBehaviorCollection;
+                    if (value == null) { return; }
+
+                    var behaviors = Interaction.GetBehaviors(sender);
+                    behaviors.Clear();
+                    foreach (var b in value.Select(x => (Behavior)x.Clone()))
+                    {
+                        behaviors.Add(b);
+                    }
+                }));
+
+        public static StyleBehaviorCollection GetStyleBehaviors(DependencyObject obj)
+        {
+            return (StyleBehaviorCollection)obj.GetValue(StyleBehaviorsProperty);
+        }
+
+        public static void SetStyleBehaviors(DependencyObject obj, StyleBehaviorCollection value)
+        {
+            obj.SetValue(StyleBehaviorsProperty, value);
+        }
+
+        protected override Freezable CreateInstanceCore()
+        {
+            return new StyleBehaviorCollection();
+        }
+    }
+}
